e75d39a179120b4f6612ab0fdc7abfea2cd48383
OnatArslan <onatarslan13@gmail.com>
2026-09-02 17:31:20 +0300

    Conditional Bean example

--- src/main/java/com/onatarslan/springcore/workspace/WorkspaceConfiguration.java
+++ src/main/java/com/onatarslan/springcore/workspace/WorkspaceConfiguration.java
@@ -1,15 +1,45 @@
 package com.onatarslan.springcore.workspace;
 
+import com.onatarslan.springcore.idgen.TaskIdGenerator;
+import com.onatarslan.springcore.idgen.UuidIdGenerator;
+import org.slf4j.Logger;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.config.Task;
 
 import java.time.Clock;
 
 @Configuration
 public class WorkspaceConfiguration {
 
-    @Bean
+    @Bean("clock") // burada Beana isim verip inject edilen yerde Qualifier kullanabiliriz
     Clock clock() {
         return Clock.systemUTC();
     }
+
+    @Bean
+    @ConditionalOnProperty(name = "id.type", havingValue = "uuid") // matchIfMissing default false eger true ise propertie yoksa Bean yaratir
+    TaskIdGenerator uuidTaskIdGenerator() {
+        System.out.println("uuid generator created-------------------------------");
+        return new UuidIdGenerator();
+    }
+
+
+    @Bean("otherClock")
+    @Profile("dev") // dev ortaminda bu yaratilir burada "!dev" "dev | test"
+    Clock otherClock() {
+        return Clock.systemUTC();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "own.legit", havingValue = "true")
+    OwnClassBeanExample ownClassBeanExample(WorkspaceRegistry registry, WorkspaceService service) {
+        System.out.println("own class example working");
+        return new OwnClassBeanExample(registry, service, "example-prefix");
+    }
+
+
+
 }
